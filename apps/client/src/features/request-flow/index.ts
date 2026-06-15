14e08ca0a7598b1d4a1f8780b515f9c938d7567f
Omid Astaraki <omid.a.astaraki@gmail.com>
2026-06-15 18:29:14 +0300

    docs(request-flow): sync C.6/E with null-id reconcile; export Episode/EpisodeStatus from barrel

--- apps/client/src/features/request-flow/index.ts
+++ apps/client/src/features/request-flow/index.ts
@@ -2,7 +2,7 @@ export { MovieRequestAction } from "./components/movie-request-action";
 export { RequestableSeasons } from "./components/requestable-seasons";
 export { requestsApi } from "./lib/fetchers";
 export { requestFlowKeys } from "./lib/query-keys";
-export type { Season } from "./lib/types";
+export type { Episode, EpisodeStatus, Season } from "./lib/types";
 export { REQUEST_TARGETS_STALE_MS, useRequestTargets } from "./hooks/use-request-targets";
 export { REQUEST_HISTORY_STALE_MS, useUserRequests } from "./hooks/use-user-requests";
 export { useCancelRequest } from "./hooks/use-cancel-request";
